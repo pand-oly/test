62577c1968c00deab0a81ea7bc9ef910bef5ce10
pand-oly <hoa98hoa@gmail.com>
2022-11-22 12:40:39 -0300

    feat(Add Ts and Prisma): Add flies in ts and prisma

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+    this.app.get('/', (req, res) => res.json({ ok: true }));
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
+
+export { App };
+
+export const { app } = new App();
