ecde7cafa1aace537ade03d1373dbd62e7ddd06f
pand-oly <hoa98hoa@gmail.com>
2022-11-29 10:55:34 -0300

    feat(App.ts): use route register and created method config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,30 @@
+import 'express-async-errors';
 import express from 'express';
+import errorHandler from './middleware/errorHandle';
+import registerRoute from './routes';
 
 class App {
   public app: express.Express;
 
   constructor() {
     this.app = express();
-    this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.config();
+
+    this.app.use(registerRoute);
+
+    this.app.use(errorHandler);
+  }
+
+  private config():void {
+    const accessControl: express.RequestHandler = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(express.json());
+    this.app.use(accessControl);
   }
 
   public start(PORT: string | number):void {
