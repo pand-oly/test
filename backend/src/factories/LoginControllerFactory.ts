4a603620f0b50b9ae928635fbd07d719c7a16dea
pand-oly <hoa98hoa@gmail.com>
2022-12-08 09:53:18 -0300

    refactor(Login page): Refactore login page to fit the test requirements

--- backend/src/factories/LoginControllerFactory.ts
+++ backend/src/factories/LoginControllerFactory.ts
@@ -1,14 +1,21 @@
 import LoginController from '../controllers/LoginController';
 import PrismaUserModel from '../prismaDatabase/models/PrismaUserModel';
-import { jwtTokenProvider, md5PasswordProvider } from '../providers';
 import UserRepository from '../repositories/implementations/UserRepository';
 import LoginService from '../services/LoginService';
+import {
+  jwtTokenProvider, md5PasswordProvider, zodAccessValidatorProvider,
+} from '../providers';
 
 export default class LoginControllerFactory {
   static make() {
     const userModel = new PrismaUserModel();
     const loginRepository = new UserRepository(userModel);
-    const loginService = new LoginService(md5PasswordProvider, jwtTokenProvider, loginRepository);
+    const loginService = new LoginService(
+      md5PasswordProvider,
+      jwtTokenProvider,
+      loginRepository,
+      zodAccessValidatorProvider,
+    );
     const loginController = new LoginController(loginService);
 
     return loginController;
