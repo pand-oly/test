34470d405c25e0c26f80c11b137cc3794bac1de6
pand-oly <hoa98hoa@gmail.com>
2023-01-06 19:08:45 -0300

    feat(Position and style container home):

--- frontend/src/components/ContainerHome.jsx
+++ frontend/src/components/ContainerHome.jsx
@@ -1,19 +1,27 @@
+import '../styles/home.css';
+
 export default function ContainerHome() {
   return (
-    <div>
-      <div>
-        <button type="button">Transaction</button>
+    <div className="main-container-home">
+      <div className="container-btn-home">
+        <button type="button" className="btn-home">
+          Transaction
+        </button>
       </div>
       <div>
-        history
-        <div>
-          <div>
+        <h3 className="title-history">history</h3>
+        <div className="container-filter-history">
+          <div className="container-input-search">
             <input type="text" /> <button type="button">search</button>
           </div>
-          <button type="button">cash-in</button>
-          <button type="button">cash-out</button>
+          <button type="button" className="btn-filter-history">
+            cash-in
+          </button>
+          <button type="button" className="btn-filter-history">
+            cash-out
+          </button>
         </div>
-        <div>storico</div>
+        <div>historico</div>
       </div>
     </div>
   );
