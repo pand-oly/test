0862abb3b924335b5dd11388c90146b66c9bc608
pand-oly <hoa98hoa@gmail.com>
2023-01-07 11:34:16 -0300

    feat(Header): Use contex in header to set user info

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import MainContext from '../context/MainContext';
 
 import '../styles/header.css';
 import pigBank from '../public/icons/cofrinho.png';
 
 export default function Header() {
   const [seeBalance, setSeeBalance] = useState(false);
+  const { user } = useContext(MainContext);
+
   return (
     <section className="main-container-header">
       <img alt="icon-pig-bank" src={pigBank} className="icon-header" />
       <div>
-        Welcome: <span>username</span>
+        Welcome: <span>{user.username}</span>
       </div>
       <div>
-        Account: <sapn>account ID</sapn>
+        Account: <sapn>{user.accountId}</sapn>
       </div>
       <div>
         Balance:{' '}
