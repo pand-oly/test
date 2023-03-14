10abcd7572650c2dd0b7ab83a3ed6adda5b5f80a
pand-oly <hoa98hoa@gmail.com>
2023-03-14 18:40:35 -0300

    feat(Card history): Add pagination functionality to cardHistory display.

--- frontend-ts/src/components/ContainerHome.tsx
+++ frontend-ts/src/components/ContainerHome.tsx
@@ -6,14 +6,18 @@ import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import type { HistoryTransaction } from '../entities/transaction';
 import type { User } from '../entities/user';
-
+import type { MouseEventHandler } from 'react';
 import '../styles/home.css';
 
+const CARDS_PER_PAGE = 2;
+
 export default function ContainerHome() {
-  const [historyTransactions, setHistory] = useState<HistoryTransaction[]>();
+  const [historyTransactions, setHistory] = useState<HistoryTransaction[]>([]);
   const [user, setUser] = useState<User>();
   const [token, setTokenState] = useState<string>('');
   const [errorHistory, setErrorHistory] = useState<string>();
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState<number>(CARDS_PER_PAGE);
 
   const navigate = useNavigate();
 
@@ -45,6 +49,20 @@ export default function ContainerHome() {
     fetchData();
   }, [user, fetchData]);
 
+  const handleCardHistory =
+    (buttonType: 'next' | 'prev'): MouseEventHandler<HTMLButtonElement> =>
+    (event) => {
+      event.preventDefault();
+
+      if (buttonType === 'next' && endIndex < historyTransactions.length) {
+        setStartIndex(endIndex);
+        setEndIndex(endIndex + CARDS_PER_PAGE);
+      } else if (buttonType === 'prev' && startIndex > 0) {
+        setStartIndex(startIndex - CARDS_PER_PAGE);
+        setEndIndex(endIndex - CARDS_PER_PAGE);
+      }
+    };
+
   return errorHistory ? (
     <p>{errorHistory}</p>
   ) : (
@@ -52,23 +70,29 @@ export default function ContainerHome() {
       <ModalTransaction />
       <div>
         <h3 className="title-history">history</h3>
-        <div className="container-filter-history">
-          <div className="container-input-search">
+        <div>
+          <div>
             <input type="text" />
             <button type="button">search</button>
           </div>
-          <button type="button" className="btn-filter-history">
-            cash-in
-          </button>
-          <button type="button" className="btn-filter-history">
-            cash-out
-          </button>
+          <button type="button">cash-in</button>
+          <button type="button">cash-out</button>
         </div>
         <div className="container-card-history">
           {historyTransactions &&
-            historyTransactions.map((transaction, index) => (
-              <CardHistory transaction={transaction} key={index} />
-            ))}
+            historyTransactions
+              .slice(startIndex, endIndex)
+              .map((transaction, index) => (
+                <div className="card-history-inline" key={index}>
+                  <CardHistory transaction={transaction} />
+                </div>
+              ))}
+          <button type="button" onClick={handleCardHistory('prev')}>
+            prev
+          </button>
+          <button type="button" onClick={handleCardHistory('next')}>
+            next
+          </button>
         </div>
       </div>
     </div>
