c48ddd5fbf5f834e73aaeee408fd52ad33ca4e5a
pand-oly <hoa98hoa@gmail.com>
2023-03-17 12:54:09 -0300

    fix(Docker compose and prestart.sh): Add front end in docker-compose fro production and in docker-compose for development update ports and add environments

--- prestart_app.sh
+++ prestart_app.sh
@@ -6,5 +6,12 @@ cacheFolderBack="/tmp/backend-cache"
 rm -rf $cacheFolderBack
 npm_config_loglevel=silent npm i --prefix ${backFolder} --cache $cacheFolderBack
 
+printf "\n> Instalando o front-end\n"
+frontFolder="./frontend"
+cacheFolderFront="/tmp/frontend-cache"
+rm -rf $cacheFolderFront
+npm_config_loglevel=silent npm i --prefix ${frontFolder} --cache $cacheFolderFront
+npm_config_loglevel=silent npm run build --prefix ${frontFolder} --cache $cacheFolderFront
+
 printf "\n Lifting containers\n"
 npm run compose-up
